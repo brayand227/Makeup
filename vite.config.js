c4bb03207525aa912384f49cd8bab77bf9d4bfab
brayand227 <brayancalle227@gmail.com>
2026-05-02 11:43:41 -0500

    cambio para cloudflare

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Makeup/'
+  // No necesitas 'base' para Cloudflare Pages
+  server: {
+    host: '127.0.0.1'
+  }
 })
